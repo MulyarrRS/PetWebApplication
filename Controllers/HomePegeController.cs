668be0458a20f8ab04686553fc73e5cf53e995c9
22222 <1111>
2021-09-12 23:58:12 +0300

    Поменял название контроллера

--- Controllers/HomePegeController.cs
+++ Controllers/HomePegeController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PetWebApplication.Controllers
 {
-    public class HomeController : Controller
+    public class HomePageController : Controller
     {
         public IActionResult Index()
         {
